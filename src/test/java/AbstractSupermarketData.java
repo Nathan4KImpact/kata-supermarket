d83c431a6f645b3d09731b62a5905bbfa8b2e55c
Nathan4KImpact <nathanaeltalla@hotmail.com>
2022-03-22 18:07:38 +0100

    Allegement des methodes de tests en deleguant l'initialisation du test fixture dans la méthode setUp() et les propriétés de base dans la classe Abstraite AbstractSupermarketData.java

--- src/test/java/AbstractSupermarketData.java
+++ src/test/java/AbstractSupermarketData.java
@@ -1,9 +1,22 @@
+import com.mss.supermarket.business.ComputeOrderService;
+import com.mss.supermarket.model.Order;
 import com.mss.supermarket.utils.EnumPricingSchemeType;
 import com.mss.supermarket.model.Product;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 public abstract class AbstractSupermarketData {
+
+    final static Float POUND_TO_OUNCE_RATE = 1F/16F;
+
+    Map<Product, Object> currPrdDetails;
+    ComputeOrderService computeOrderService;
+
+    Product computer, cup, flour, chocolateBar;
+    Order ord0,ord1,ord2,ord3,ord4,ord5,ord6,ord7,ord8,ord9,ord10;
+
+
     public Product getProduct(String productLabel, String price, EnumPricingSchemeType princingType){
         return new Product(productLabel, new BigDecimal(price), princingType);
     }
